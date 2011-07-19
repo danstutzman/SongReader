f075e191212a3ba7442693ffce46bf6b4b59cab1
Daniel Stutzman <daniel@diagonalsoftware.com>
2011-07-19 15:37:49 -0400

    Refactored to be object-oriented.

--- read.py
+++ read.py
@@ -35,76 +35,100 @@ height = matrix.shape[0]
 root = Tk()
 
 im = PIL.Image.fromstring('L', (width, height), matrix.astype('b').tostring())
-im_resized = None
-photo = PIL.ImageTk.PhotoImage(image=im)
 
-frame = Frame(root, bd=2, relief=SUNKEN)
-frame.grid_rowconfigure(0, weight=1)
-frame.grid_columnconfigure(0, weight=1)
+class Frame2(Frame):
+  def __init__(self, root, pil_image, canvas_w, canvas_h):
+    Frame.__init__(self, root, bd=2, relief=SUNKEN)
+    self.canvas_w = canvas_w
+    self.canvas_h = canvas_h
+    self.zoom = 1
+    self.pil_image = pil_image
+    self.image_w, self.image_h = pil_image.size
+    self.image_num = None
 
-xscrollbar = Scrollbar(frame, orient=HORIZONTAL)
-xscrollbar.grid(row=1, column=0, sticky=E+W)
+    self.grid_rowconfigure(0, weight=1)
+    self.grid_columnconfigure(0, weight=1)
+    self.xscrollbar = Scrollbar(self, orient=HORIZONTAL)
+    self.xscrollbar.grid(row=1, column=0, sticky=E+W)
+    self.yscrollbar = Scrollbar(self)
+    self.yscrollbar.grid(row=0, column=1, sticky=N+S)
+    self.canvas = Canvas(self,
+      width=self.canvas_w, height=self.canvas_h,
+      scrollregion=(0, 0, self.image_w, self.image_h))
+    self.scroll_x, self.scroll_y = 0.0, 0.0
 
-yscrollbar = Scrollbar(frame)
-yscrollbar.grid(row=0, column=1, sticky=N+S)
+    self.canvas.grid(row=0, column=0, sticky=N+S+E+W)
+    self.canvas.config(yscrollcommand=self.yscrollbar.set)
+    self.canvas.config(xscrollcommand=self.xscrollbar.set)
+    
+    self.xscrollbar.config(command=self.xview)
+    self.yscrollbar.config(command=self.yview)
 
-canvas = Canvas(frame, width=300, height=300, \
-  scrollregion = (0, 0, im.size[0], im.size[1]))
-image_num = canvas.create_image(0,0, image=photo, anchor=NW)
-zoom = 1
+  def scroll_w(self):
+    return (float)(self.canvas_w) / self.image_w / self.zoom
+  def scroll_h(self):
+    return (float)(self.canvas_h) / self.image_h / self.zoom
 
-def update_image():
-  global canvas, image_num, photo, im_resized, zoom, scroll_x, scroll_y, yscrollbar
-  canvas.delete(image_num)
+  def update_image(self):
+    if self.image_num:
+      self.canvas.delete(self.image_num)
+  
+    if self.scroll_x < 0.0:
+      self.scroll_x = 0.0
+    if self.scroll_x > 1.0 - self.scroll_w():
+      self.scroll_x = 1.0 - self.scroll_w()
+    if self.scroll_y < 0.0:
+      self.scroll_y = 0.0
+    if self.scroll_y > 1.0 - self.scroll_h():
+      self.scroll_y = 1.0 - self.scroll_h()
 
-  iw, ih = im.size
-  cw, ch = (int)(iw / zoom), (int)(ih / zoom)
-  _x, _y = (int)(scroll_x * iw), (int)(scroll_y * ih)
-  canvas_w = (int)(canvas.config()['width'][4])
-  canvas_h = (int)(canvas.config()['height'][4])
-  crop_window = (_x, _y, _x + (canvas_w / zoom), _y + (canvas_h / zoom))
-  im_resized = im.crop(crop_window).resize((canvas_w, canvas_h))
+    source_x = (int)(self.scroll_x * self.image_w)
+    source_y = (int)(self.scroll_y * self.image_h)
+    crop_window = (source_x, source_y,
+      source_x + (self.canvas_w / self.zoom),
+      source_y + (self.canvas_h / self.zoom))
+    im_resized = im.crop(crop_window).resize((self.canvas_w, self.canvas_h))
+  
+    self.photo = PIL.ImageTk.PhotoImage(image=im_resized)
+    self.image_num = self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
+    self.xscrollbar.set(self.scroll_x, self.scroll_x + self.scroll_w())
+    self.yscrollbar.set(self.scroll_y, self.scroll_y + self.scroll_h())
 
-  photo = PIL.ImageTk.PhotoImage(image=im_resized)
-  image_num = canvas.create_image(0, 0, image=photo, anchor=NW)
-  xscrollbar.set(scroll_x, scroll_x + ((float)(canvas_w) / im.size[0] / zoom))
-  yscrollbar.set(scroll_y, scroll_y + ((float)(canvas_h) / im.size[1] / zoom))
+  def xview(self, arg1, arg2=None, arg3=None):
+    if arg1 == 'moveto':
+      self.scroll_x = float(arg2)
+      self.update_image()
+    elif arg1 == 'scroll':
+      if arg3 == 'pages':
+        self.scroll_x += int(arg2) * self.scroll_w()
+      elif arg3 == 'units':
+        self.scroll_x += float(arg2) * 50 / self.zoom / self.image_w
+      self.update_image()
+    else:
+      print ('unknown command to xview', arg1, arg2, arg3)
+  def yview(self, arg1, arg2=None, arg3=None):
+    if arg1 == 'moveto':
+      self.scroll_y = float(arg2)
+      self.update_image()
+    elif arg1 == 'scroll':
+      if arg3 == 'pages':
+        self.scroll_y += int(arg2) * self.scroll_h()
+      elif arg3 == 'units':
+        self.scroll_y += float(arg2) * 50 / self.zoom / self.image_h
+      self.update_image()
+    else:
+      print ('unknown command to yview', arg1, arg2, arg3)
 
-scroll_x = 0.0 # position between 0 and 1 where left of scroll bar is
-scroll_y = 0.0 # position between 0 and 1 where top of scroll bar is
-def xview(arg1, arg2=None, arg3=None):
-  global canvas, scroll_x, scroll_y
-  if arg1 == 'moveto':
-    scroll_x = float(arg2)
-    update_image()
-def yview(arg1, arg2=None, arg3=None):
-  global canvas, scroll_x, scroll_y
-  if arg1 == 'moveto':
-    scroll_y = float(arg2)
-    update_image()
+frame2 = Frame2(root, im, 500, 500)
+frame2.pack(side=LEFT)
 
-canvas.grid(row=0, column=0, sticky=N+S+E+W)
-canvas.config(yscrollcommand=yscrollbar.set)
-canvas.config(xscrollcommand=xscrollbar.set)
+def scale_changed(multiplier):
+  frame2.zoom = int(multiplier)
+  frame2.update_image()
 
-xscrollbar.config(command=xview)
-yscrollbar.config(command=yview)
-
-frame.pack(side=LEFT)
-
-def scale_changed(multiplier): # 0 to 100
-  # im_resized needs to be global for some reason,
-  # maybe because otherwise it's incorrectly garbage collected?
-  global im, im_resized, image_num, canvas, photo, zoom
-  multiplier = int(multiplier)
-  zoom = multiplier
-  update_image()
-
-frame2 = Frame(root).pack(side=LEFT)
-Scale(frame2, orient=HORIZONTAL, command=scale_changed, from_=1, to=2).pack()
-Label(frame2, text='here').pack()
-Button(frame2, text='left').pack()
-Button(frame2, text='right').pack()
+frame3 = Frame(root).pack(side=LEFT)
+Label(frame3, text='Zoom').pack()
+Scale(frame3, orient=HORIZONTAL, command=scale_changed, from_=1, to=5).pack()
 
 root.update()
 root.mainloop()
