eda89d53ccce121bd7435edfc24f4e0c7419b22c
Daniel Stutzman <daniel@diagonalsoftware.com>
2011-07-28 13:36:56 -0600

    Labeling boxes, although saving the emitted JSON must be done by hand.

--- server.rb
+++ server.rb
@@ -10,3 +10,10 @@ end
 get '/photo.jpeg' do
   send_file 'photo.jpeg'
 end
+get '/boxes.json' do
+  content_type :json
+  send_file 'boxes.json'
+end
+get '/json2.js' do
+  send_file 'json2.js'
+end
