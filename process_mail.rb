e887702d161f4175c087a86d3d49115489c796a7
Daniel Stutzman <daniel@diagonalsoftware.com>
2011-11-02 13:28:44 -0600

    Process_mail now converts the midi to wav, and the wav to mp3

--- process_mail.rb
+++ process_mail.rb
@@ -5,6 +5,34 @@ require 'time'
 require './smtp_tls_patch.rb'
 require 'open3'
 
+Mail.defaults do
+  delivery_method :smtp, {
+    :address              => 'smtp.gmail.com',
+    :port                 => 587,
+    :domain               => 'localhost',
+    :user_name            => 'beta@songreader.net',
+    :password             => `cat ./email_password_for_smtp`,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true,
+    :openssl_verify_mode  => nil
+  }
+end
+
+class GotNonZeroExitCode < StandardError
+end
+
+def run_command(command)
+  puts command
+  output = ''
+  stdout, stderr, status = Open3.capture3(command)
+  output = (stdout + stderr != '') ? stdout + stderr : 'No output'
+  puts stdout
+  puts stderr
+  if status.exitstatus != 0
+    raise GotNonZeroExitCode.new(status.exitstatus)
+  end
+end
+
 create_dirs = false
 maildir = Maildir.new('getmail', create_dirs)
 maildir.list(:new).each { |message|
@@ -20,53 +48,27 @@ maildir.list(:new).each { |message|
         file.write("[]\n")
       }
 
-      command = "bin/run Ocr4Music #{message.unique_name}.#{i}"
-      #command = "exit 2"
-      puts command
-      output = ''
-      stdout, stderr, status = Open3.capture3(command)
-      output = (stdout + stderr != '') ? stdout + stderr : 'No output'
-      puts "Got error code of #{status.exitstatus}"
-
-      if status.exitstatus != 0
-        puts 'Sending error email...'
-        from = 'beta@songreader.net'
-        to = 'dtstutz@gmail.com'
-        #content = ''
-        #content += "From: #{from}\n"
-        #content += "To: #{to}\n"
-        #content += "Subject: Error from Song Reader\n"
-        #content += "Date: #{Time.now.rfc2822}\n"
-        #content += "\n"
-        #content += output
-        #Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-        #Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com',
-        #    'beta@songreader.net', 'ddvpgiqgorvibcva', :login) do |smtp|
-        #  smtp.send_message(content, from, to)
-        #end
-        
-        Mail.defaults do
-          delivery_method :smtp, {
-            :address              => 'smtp.gmail.com',
-            :port                 => 587,
-            :domain               => 'localhost',
-            :user_name            => 'beta@songreader.net',
-            :password             => `cat ./email_password_for_smtp`,
-            :authentication       => 'plain',
-            :enable_starttls_auto => true,
-            :openssl_verify_mode  => nil
-          }
+      begin
+        case_name = "#{message.unique_name}.#{i}"
+        run_command("bin/run Ocr4Music #{case_name}")
+        run_command("bin/midi2wav output/midi/#{case_name}.mid " +
+                    "output/wav/#{case_name}.wav")
+        run_command("bin/wav2mp3 #{case_name}")
+        run_command("rm output/wav/#{case_name}.wav")
+      rescue Exception => e
+        if status.exitstatus != 0 # if failure
+          #puts 'Sending error email...'
+          #error_mail = Mail.new do
+          #  from 'error@songreader.net'
+          #  to 'dtstutz@gmail.com'
+          #  subject 'Error from Song Reader'
+          #  body output
+          #end
+          #error_mail.deliver!
+          puts 'Quitting because of error'
+          raise
+  # EARLY EXIT
         end
-        error_mail = Mail.new do
-          from 'error@songreader.net'
-          to 'dtstutz@gmail.com'
-          subject "Error from Song Reader"
-          body output
-        end
-        error_mail.deliver!
-        puts "Quitting..."
-# EARLY EXIT
-        exit status.exitstatus
       end
     else
       puts "Skipped attachment with mime type #{attachment.mime_type}"
