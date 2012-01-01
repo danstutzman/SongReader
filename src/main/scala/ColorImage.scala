b64975d9239d608d83f63d7333372e04bd6ef879
Daniel Stutzman <daniel@diagonalsoftware.com>
2011-12-31 20:00:41 -0700

    Auto-rotate the JPEG based on orientation metadata, using Sanselan image library.

--- src/main/scala/ColorImage.scala
+++ src/main/scala/ColorImage.scala
@@ -1,7 +1,14 @@
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.io.File
+import java.lang.Math
 import javax.imageio.ImageIO
+import org.apache.sanselan.Sanselan
+import org.apache.sanselan.common.IImageMetadata
+import org.apache.sanselan.formats.jpeg.JpegImageMetadata
+import org.apache.sanselan.formats.tiff.TiffField
+import org.apache.sanselan.formats.tiff.TiffImageMetadata
+import org.apache.sanselan.formats.tiff.constants.TagInfo
 
 class ColorImage(val w:Int, val h:Int, val data:Array[(Int,Int,Int)]) {
   def this(w:Int, h:Int) = this(w, h, ColorImage.allocateBlankData(w * h))
@@ -40,14 +47,23 @@ class ColorImage(val w:Int, val h:Int, val data:Array[(Int,Int,Int)]) {
     }
   }
   def saveTo(file:File) {
-    def convertRGBTupleToARGBInt(rgb:(Int,Int,Int)) : Int = {
+    def convertRGBTupleToRGBInt(rgb:(Int,Int,Int)) : Int = {
       val (r, g, b) = rgb
-      (255 << 24) + (r << 16) + (g << 8) + (b << 0)
+      (0 << 24) + (r << 16) + (g << 8) + (b << 0)
     }
-    val imageOut = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
-    val pixelsARGBNew = data.map { convertRGBTupleToARGBInt }
-    imageOut.setRGB(0, 0, w, h, pixelsARGBNew, 0, w)
-    ImageIO.write(imageOut, "PNG", file)
+    val imageOut = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB)
+    val pixelsRGBNew = data.map { convertRGBTupleToRGBInt }
+    imageOut.setRGB(0, 0, w, h, pixelsRGBNew, 0, w)
+
+    if (file.getName().endsWith(".png"))
+      ImageIO.write(imageOut, "PNG", file)
+    else if (file.getName().endsWith(".jpg"))
+      ImageIO.write(imageOut, "JPEG", file)
+    else if (file.getName().endsWith(".jpeg"))
+      ImageIO.write(imageOut, "JPEG", file)
+    else
+      throw new RuntimeException(
+        "Don't know what format %s is".format(file.getName()))
   }
 }
 
@@ -57,6 +73,63 @@ object ColorImage {
     (0 until length).foreach { i => data(i) = (0,0,0) }
     data
   }
+  def findRotationNeeded(file:File) : Int = {
+    Sanselan.getMetadata(file) match {
+      case jpegMetadata:JpegImageMetadata =>
+        val items = jpegMetadata.getItems()
+        (0 until items.size()).foreach { i =>
+          items.get(i) match {
+            case item:TiffImageMetadata.Item =>
+              if (item.getTiffField.getTagName == "Orientation") {
+                return item.getTiffField.getValueDescription match {
+                  case "1" => 0
+                  case "6" => 90
+                  case "3" => 80
+                  case "8" => 270
+                  case   _ => 0
+                }
+              }
+            case _ => ()
+          }
+        }
+        case _ => {}
+    }
+    return 0
+  }
+  def rotateBufferedImage(unrotatedImage:BufferedImage, rotation:Int) = {
+    rotation match {
+      case 0 =>
+        unrotatedImage
+      case 90 =>
+        val imageOut = new BufferedImage(
+          unrotatedImage.getHeight(), unrotatedImage.getWidth(),
+          BufferedImage.TYPE_INT_ARGB)
+        val g2d = imageOut.getGraphics().asInstanceOf[Graphics2D]
+        g2d.rotate(Math.toRadians(90.0))
+        g2d.drawImage(unrotatedImage, 0, -unrotatedImage.getHeight(), null)
+        g2d.dispose()
+        imageOut
+      case 270 =>
+        val imageOut = new BufferedImage(
+          unrotatedImage.getHeight(), unrotatedImage.getWidth(),
+          BufferedImage.TYPE_INT_ARGB)
+        val g2d = imageOut.getGraphics().asInstanceOf[Graphics2D]
+        g2d.rotate(Math.toRadians(270.0))
+        g2d.drawImage(unrotatedImage, -unrotatedImage.getWidth(), 0, null)
+        g2d.dispose()
+        imageOut
+      case 180 =>
+        val imageOut = new BufferedImage(
+          unrotatedImage.getWidth(), unrotatedImage.getHeight(),
+          BufferedImage.TYPE_INT_ARGB)
+        val g2d = imageOut.getGraphics().asInstanceOf[Graphics2D]
+        g2d.rotate(Math.toRadians(180.0))
+        g2d.drawImage(unrotatedImage,
+          -unrotatedImage.getWidth(), -unrotatedImage.getHeight(), null)
+        g2d.dispose()
+        imageOut
+    }
+  }
   def readFromFile(file:File) : ColorImage = {
     def convertARGBIntToRGBTuple(argb:Int) : (Int,Int,Int) = {
       val a = (argb >> 24) & 0xff
@@ -65,7 +138,9 @@ object ColorImage {
       val b = (argb >> 0) & 0xff
       (r, g, b)
     }
-    val bufferedImage = ImageIO.read(file)
+    val unrotatedImage = ImageIO.read(file)
+    val rotation = findRotationNeeded(file)
+    val bufferedImage = rotateBufferedImage(unrotatedImage, rotation)
     val w = bufferedImage.getWidth()
     val h = bufferedImage.getHeight()
     val pixelsARGB:Array[Int] = bufferedImage.getRGB(0, 0, w, h, null, 0, w)
